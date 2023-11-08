e42ac79e4f14323d73a31549a0bd6cf628db430e
Ronita Adhikari <ronitaadhikari555@gmail.com>
2023-11-08 08:26:30 +0530

    Added a comboBox: religion

--- Bank Management System(Complete)/src/app/Form_2.java
+++ Bank Management System(Complete)/src/app/Form_2.java
@@ -5,6 +5,7 @@
 package app;
 
 import java.awt.Font;
+import javax.swing.JComboBox;
 //import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,6 +27,12 @@ public class Form_2 extends JFrame {
         label1.setBounds(100,45,300,50);
         label1.setFont(new Font("Raleway",Font.BOLD,25));
         add(label1);
+        
+        String[] religion = {"Hindu","Buddhist","Muslim","Jewish","Sikh","Other"};
+        JComboBox religion_combo=new JComboBox(religion);
+        religion_combo.setBounds(350,55,400,30);
+        religion_combo.setFont(new Font("Raleway",Font.PLAIN,20));
+        add(religion_combo);
         
         
         
